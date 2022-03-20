bb5f34ccd49fb148142ac6c244494d8939c0e88d
ooussem <oussem93@hotmail.fr>
2022-03-20 16:29:54 +0100

    Add map in Applicative

--- src/main/scala/com/danielasfregola/tutorial/cat/applicative/ApplicativeInstances.scala
+++ src/main/scala/com/danielasfregola/tutorial/cat/applicative/ApplicativeInstances.scala
@@ -6,8 +6,22 @@ import com.danielasfregola.tutorial.cat._
 
 object ApplicativeInstances {
 
-  implicit val maybeApplicative: Applicative[Maybe] = ???
+  implicit val maybeApplicative: Applicative[Maybe] = new Applicative[Maybe] {
+    override def pure[A](a: A): Maybe[A] = Just(a)
 
-  implicit val zeroOrMoreApplicative: Applicative[ZeroOrMore] = ???
+    override def ap[A, B](boxF: Maybe[A => B])(boxA: Maybe[A]): Maybe[B] = boxF match {
+      case Just(f) => map(boxA)(f)
+      case Empty => Empty
+    }
+  }
+
+  implicit val zeroOrMoreApplicative: Applicative[ZeroOrMore] = new Applicative[ZeroOrMore] {
+    override def pure[A](a: A): ZeroOrMore[A] = OneOrMore(a, Zero)
+
+    override def ap[A, B](boxF: ZeroOrMore[A => B])(boxA: ZeroOrMore[A]): ZeroOrMore[B] = boxF match {
+      case OneOrMore(head, tail) => map(ap(tail)(boxA))(head)
+      case Zero => Zero
+    }
+  }
 
 }
